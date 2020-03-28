519e5df1bf1a8bbbf3d0f846e62d9b4ee4a324b3
tanakaXD <ttnk.0112@gmail.com>
2020-03-28 23:32:13 +0900

    1-4 tweek

---
+++ stopwatch/js/k-challenge-stuff.js
@@ -0,0 +1,24 @@
+function prime_number_generator(num) {
+    let primes = [];
+    for (let i = 2; i < 10000; i++) {
+        let prime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                prime = false;
+                break;
+            }
+        }
+        if (prime) {
+            primes.push(i);
+        }
+
+        if (primes.length == num) {
+            return primes;
+        }
+
+    }
+
+}
+
+
+console.log(prime_number_generator(100));
